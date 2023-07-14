cfd20d254397edd2a56a14640c130186380f42d0
cchinchole <cpchinchole@gmail.com>
2023-07-14 02:36:15 -0500

    Cleaned up

--- rsa/inc/defs.hpp
+++ rsa/inc/defs.hpp
@@ -1,7 +1,7 @@
-//#define LOG_CRYPTO
 #define DO_CHECKS
 #define LOG_PRIME_GEN_B_3_3
 #define LOG_PRIME_GEN_B_3_6
+#define LOG_CRYPTO
 //#define LOG_PARAMS
 
 #ifndef __func__
--- rsa/main.cpp
+++ rsa/main.cpp
@@ -21,6 +21,7 @@
 #include "inc/defs.hpp"
 #include "inc/rsa.hpp"
 #include "inc/primes.hpp"
+#include "inc/rand.hpp"
 
 const int kBits = 2048;
 int keylen;
@@ -69,53 +70,12 @@ int testPrimesBetweenFuncs()
   return 0;
 }
 
-#define DRNG_NO_SUPPORT 0x0 /* For clarity */
-#define DRNG_HAS_RDRAND 0x1
-#define DRNG_HAS_RDSEED 0x2
 
-
-// These don't need to do anything if you don't have anything for them to do.
-static void stdlib_rand_cleanup() {}
-static int stdlib_rand_add(const void *buf, int num, double add_entropy) {return 0;}
-static int stdlib_rand_status() { return 1; }
-
-// Seed the RNG.  srand() takes an unsigned int, so we just use the first
-// sizeof(unsigned int) bytes in the buffer to seed the RNG.
-static int stdlib_rand_seed(const void *buf, int num)
-{
-        assert(num >= sizeof(unsigned int));
-        srand( *((unsigned int *) buf) );
-        return 0;
-}
-
-// Fill the buffer with random bytes.  For each byte in the buffer, we generate
-// a random number and clamp it to the range of a byte, 0-255.
-static int stdlib_rand_bytes(unsigned char *buf, int num)
-{
-        for( int index = 0; index < num; ++index )
-        {
-                buf[index] = rand() % 256;
-        }
-        return 1;
-}
-
-
-// Create the table that will link OpenSSL's rand API to our functions.
-RAND_METHOD stdlib_rand_meth = {
-        stdlib_rand_seed,
-        stdlib_rand_bytes,
-        stdlib_rand_cleanup,
-        stdlib_rand_add,
-        stdlib_rand_bytes,
-        stdlib_rand_status
-};
-
-// This is a public-scope accessor method for our table.
-RAND_METHOD *RAND_stdlib() { return &stdlib_rand_meth; }
 
 
 int main(int argc, char *argv[]) {
   
+
 BIGNUM* myE = BN_new();
 BN_set_word(myE, 0x100000001);
 
@@ -134,9 +94,10 @@ cRSA *myRsa = new cRSA(kBits, myE, true);
 
 BIGNUM *bnLongRand = BN_secure_new();
 BN_rand_ex(bnLongRand, 1024, BN_RAND_TOP_ANY, BN_RAND_BOTTOM_ANY, 0, BN_CTX_secure_new());
+unsigned char* testBytes = (unsigned char*)malloc(32*sizeof(char));
+RAND_bytes(testBytes, 32);
 roundTrip(myRsa, (char*)"Test string HeRe! HelLO WoRLd!@#$^&*()_+ 1   2 34    567  89\nTest!");
 printf("\n\nTesting long string now.\n\n");
 roundTrip(myRsa, (char*)BN_bn2dec(bnLongRand));
-_Logger->info("test info!");
 return 0;
 }
