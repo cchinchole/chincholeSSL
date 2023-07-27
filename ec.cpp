b410dd5f3ba449e4ea35808d81f88c959fd04650
cchinchole <cpchinchole@gmail.com>
2023-07-27 00:33:40 -0500

    Cleaned up math functions (no efficiency improvements)

--- ec.cpp
+++ ec.cpp
@@ -39,43 +39,53 @@ bool isPointAtInfinity(cECPoint *p)
     return ( BN_is_zero(p->x) && BN_is_zero(p->y) );
 }
 
-cECPoint *getPointAtInfinity(cECPoint *p)
+void setPointToInfinity(cECPoint *p)
 {
-    BN_set_word(p->x, 0);
-    BN_set_word(p->y, 0);
-    return p;
+    BN_zero(p->x);
+    BN_zero(p->y);
 }
 
-void ECdouble(cECGroup *g, cECPoint *ret, cECPoint *a)
+void ECCopyPoint(cECPoint *to, cECPoint *from)
 {
-    BN_copy(ret->x, a->x);
-    BN_copy(ret->y, a->y);
+        BN_copy(to->x, from->x);
+        BN_copy(to->y, from->y);
+}
 
-    BIGNUM *tmp = BN_new();
-    BIGNUM *tmp2 = BN_new();
-    BIGNUM *tmp3 = BN_new();
-    BIGNUM *x3 = BN_dup(ret->x);
-    BIGNUM *x2 = BN_dup(ret->x);
-    BIGNUM *y2 = BN_dup(ret->y);
+void ECdouble(cECPrimeField *g, cECPoint *final_ret, cECPoint *a, BN_CTX *ctx = BN_CTX_new())
+{
+    BN_CTX_start(ctx);
+    cECPoint *ret = new cECPoint();
+    ECCopyPoint(ret, a);
+
+    BIGNUM *tmp = BN_CTX_get(ctx);
+    BIGNUM *tmp2 = BN_CTX_get(ctx);
+    BIGNUM *tmp3 = BN_CTX_get(ctx);
+    BIGNUM *x3 = BN_CTX_get(ctx);
+    BIGNUM *x2 = BN_CTX_get(ctx);
+    BIGNUM *y2 = BN_CTX_get(ctx);
+
+    BN_copy(x3, ret->x);
+    BN_copy(x2, ret->x);
+    BN_copy(y2, ret->y);
 
     BN_mul_word(x3, 3);
     BN_mul_word(x2, 2);
     BN_mul_word(y2, 2);
 
     /* (3x * x + Acurve) * modinv( 2y, Pcurve ) % Pcurve */
-    BN_mul(tmp, x3, ret->x, BN_CTX_new());
+    BN_mul(tmp, x3, ret->x, ctx);
     BN_add(tmp, tmp, g->a);                          /* tmp holds 3x^2+Acurve  */
-    BN_mod_inverse(tmp2, y2, g->p, BN_CTX_new());    /* tmp2 holds 2y^-1 mod( pCurve )*/
-    BN_mod(tmp2, tmp2, g->p, BN_CTX_new());
-    BN_mul(tmp, tmp, tmp2, BN_CTX_new());           /* tmp now holds lambda */
-    BN_mul(tmp2, tmp, tmp, BN_CTX_new());           /* tmp2 now holds lambda^2 */
+    BN_mod_inverse(tmp2, y2, g->p, ctx);    /* tmp2 holds 2y^-1 mod( pCurve )*/
+    BN_mod(tmp2, tmp2, g->p, ctx);
+    BN_mul(tmp, tmp, tmp2, ctx);           /* tmp now holds lambda */
+    BN_mul(tmp2, tmp, tmp, ctx);           /* tmp2 now holds lambda^2 */
     BN_sub(tmp3, tmp2, x2);
-    BN_mod(ret->x, tmp3, g->p, BN_CTX_new());
+    BN_mod(ret->x, tmp3, g->p, ctx);
 
     BN_sub(tmp2, a->x, ret->x);                     /* tmp2 now holds X - ret.x */
-    BN_mul(tmp, tmp, tmp2, BN_CTX_new());           /* tmp now holds lambda * (X - ret.x) */ 
+    BN_mul(tmp, tmp, tmp2, ctx);           /* tmp now holds lambda * (X - ret.x) */ 
     BN_sub(tmp, tmp, a->y);                         /* tmp now holds lambda * (X - ret.x) - Y */
-    BN_mod(ret->y, tmp, g->p, BN_CTX_new());        /* ret->y now holds ( lambda * (X - ret.x) - Y ) mod P */
+    BN_mod(ret->y, tmp, g->p, ctx);        /* ret->y now holds ( lambda * (X - ret.x) - Y ) mod P */
 
     if( BN_cmp(ret->x, BN_value_zero()) == -1 )
         BN_add(ret->x, ret->x, g->p);
@@ -83,26 +93,29 @@ void ECdouble(cECGroup *g, cECPoint *ret, cECPoint *a)
         
     if( BN_cmp(ret->y, BN_value_zero()) == -1 )
         BN_add(ret->y, ret->y, g->p);
+    BN_CTX_end(ctx);
+    BN_CTX_free(ctx);
+    ECCopyPoint(final_ret, ret);
 }
 
-cECPoint *ECadd(cECGroup *g, cECPoint *a, cECPoint *b, BN_CTX *ctx = BN_CTX_new())
+void ECadd(cECPrimeField *g, cECPoint *final_out, cECPoint *a, cECPoint *b, BN_CTX *ctx = BN_CTX_new())
 {
-    cECPoint *res = new cECPoint();
     BN_CTX_start(ctx);
     BIGNUM *tmp = BN_CTX_get(ctx);
+    cECPoint *res = new cECPoint();
     if(isPointAtInfinity(a) && isPointAtInfinity(b))
     {
-        res = getPointAtInfinity(res);
+        setPointToInfinity(res);
         goto ending;
     }
     else if(isPointAtInfinity(a))
     {
-        res = b;
+        ECCopyPoint(res, b);
         goto ending;
     }
     else if(isPointAtInfinity(b))
     {
-        res = a;
+        ECCopyPoint(res, a);
         goto ending;
     }
     else if(!BN_cmp( (a->x), (b->x)) && !BN_cmp((a->y), (b->y)) )
@@ -110,7 +123,7 @@ cECPoint *ECadd(cECGroup *g, cECPoint *a, cECPoint *b, BN_CTX *ctx = BN_CTX_new(
         BN_mod(tmp, a->y, (g->p), ctx);
         if(BN_is_zero(tmp))
         {
-            res = getPointAtInfinity(res);
+            setPointToInfinity(res);
             goto ending;
         }
         ECdouble(g, res, a);
@@ -122,7 +135,7 @@ cECPoint *ECadd(cECGroup *g, cECPoint *a, cECPoint *b, BN_CTX *ctx = BN_CTX_new(
         BN_mod(tmp, tmp, g->p, ctx);
         if( BN_is_zero(tmp) )
         {
-            res = getPointAtInfinity(res);
+            setPointToInfinity(res);
             goto ending;
         }
 
@@ -157,62 +170,89 @@ cECPoint *ECadd(cECGroup *g, cECPoint *a, cECPoint *b, BN_CTX *ctx = BN_CTX_new(
         goto ending;
     }
     ending:
+    ECCopyPoint(final_out, res);
     BN_CTX_end(ctx);
-    return res;
 }
 
-cECPoint *ECScalarMult(cECGroup *g, BIGNUM *scalar, cECPoint *Point, BN_CTX *ctx = BN_CTX_new())
+void ECScalarMult(cECPrimeField *g, cECPoint *Q_output, BIGNUM *scalar, cECPoint *Point, BN_CTX *ctx = BN_CTX_new())
 {
     BN_CTX_start(ctx);
     int i = 0;
     cECPoint *temp = new cECPoint();
     cECPoint *PointCopy = new cECPoint();
     cECPoint *QRes = new cECPoint();
-    BN_copy(PointCopy->x, Point->x);
-    BN_copy(PointCopy->y, Point->y);
-    QRes = getPointAtInfinity(QRes);
+    ECCopyPoint(PointCopy, Point);
+    setPointToInfinity(QRes);
     for(i = BN_num_bits(scalar); i>=0; i--)
     {
         if( BN_is_bit_set(scalar, i) )
-        {
-            printf("[%d / 0] %d bit is set\n", BN_num_bits(scalar), i);
             break;
-        }
     }
 
     for(int j = 0; j <= i; j++)
     {
         if( BN_is_bit_set(scalar, j) )
         {
-            printf("LOOP 2: %d bit is set\n ", j);
-            temp = ECadd(g, QRes, PointCopy);
-            BN_copy(QRes->x, temp->x);
-            BN_copy(QRes->y, temp->y);
+            ECadd(g, temp, QRes, PointCopy);
+            ECCopyPoint(QRes, temp);
         }
         ECdouble(g, temp, PointCopy);
-        BN_copy(PointCopy->x, temp->x);
-        BN_copy(PointCopy->y, temp->y);
-        printf("Result thus far in nonbit set { %s, %s }\n", BN_bn2dec(QRes->x), BN_bn2dec(QRes->y));
+        ECCopyPoint(PointCopy, temp);
     }
     BN_CTX_end(ctx);
-    return QRes;
+    ECCopyPoint(Q_output, QRes);
 }
+
+
+int test_math()
+{
+    BN_CTX *ctx = BN_CTX_new();
+    BN_CTX_start(ctx);
+    cECPrimeField *group = new PrimeTestField();
+    cECPoint *ga = new cECPoint();
+    ga->x = BN_copy(ga->x, group->G->x);
+    ga->y = BN_copy(ga->y, group->G->y);
+
+    printf("GAx: %s\n", BN_bn2dec(ga->x));
+    printf("GAy: %s\n", BN_bn2dec(ga->y));
+    cECPoint *initial13 = new cECPoint();
+    cECPoint *result = new cECPoint();
+    ECCopyPoint(initial13, ga);
+    setPointToInfinity(result);
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Result Addition {%s, %s} + ", BN_bn2dec(result->x), BN_bn2dec(result->y));
+        ECadd(group, result, initial13, result);
+        printf("{%s, %s} = { %s, %s }\n", BN_bn2dec(ga->x), BN_bn2dec(ga->y), BN_bn2dec(result->x), BN_bn2dec(result->y));
+    }
+    BIGNUM *scalar = BN_CTX_get(ctx);
+    
+    for(int i = 0; i < 10; i++)
+    {
+        BN_set_word(scalar, i);
+        ECScalarMult(group, result, scalar, initial13);
+        printf("Result Multiply {%s, %s} * %s = { %s, %s }\n", BN_bn2dec(initial13->x), BN_bn2dec(initial13->y), BN_bn2dec(scalar), BN_bn2dec(result->x), BN_bn2dec(result->y));
+    }
+    BN_CTX_end(ctx);
+    BN_CTX_free(ctx);
+    return 0;
+}
+
+
 
 int ec_generate_key(  )
 {
+    
     BN_CTX *ctx = BN_CTX_new();
     BN_CTX_start(ctx);
     cECKey key;
     key.group = new Prime256v1();
-    //key.group = new PrimeTestField();
     BIGNUM *tmp = BN_dup(key.group->n);
     BN_sub(tmp, tmp, BN_value_one());
-    printf("%s\n", BN_bn2dec(tmp));
+    printf("N:  %s\n", BN_bn2hex(key.group->n));
     printf("Gx: %s\n", BN_bn2hex(key.group->G->x));
     printf("Gy: %s\n", BN_bn2hex(key.group->G->y));
 
-
-    
     Generate:
         BN_priv_rand_range_ex(key.priv, tmp, 0, ctx);
     
@@ -220,39 +260,13 @@ int ec_generate_key(  )
     {
         goto Generate;
     }
-    key.pub = ECScalarMult(key.group, key.priv, key.group->G);
+    ECScalarMult(key.group, key.pub, key.priv, key.group->G);
     
     printf("D:  %s\n", BN_bn2hex(key.priv));
     printf("Qx: %s\n", BN_bn2hex(key.pub->x));
     printf("Qy: %s\n", BN_bn2hex(key.pub->y));
     
-    /*
-    cECPoint *ga = new cECPoint();
-    ga->x = BN_copy(ga->x, key.group->G->x);
-    ga->y = BN_copy(ga->y, key.group->G->y);
-
-    printf("GAx: %s\n", BN_bn2dec(ga->x));
-    printf("GAy: %s\n", BN_bn2dec(ga->y));
-    cECPoint *ret = new cECPoint();
-    cECPoint *result = new cECPoint();
-    BN_copy(ret->x, ga->x);
-    BN_copy(ret->y, ga->y);
-    result = ECadd(key.group, ret, ga);
-    for(int i = 0; i < 5; i++)
-    {
-        printf("Result Addition {%s, %s} + ", BN_bn2dec(result->x), BN_bn2dec(result->y));
-        result = ECadd(key.group, result, ga);
-        printf("{%s, %s} = { %s, %s }\n", BN_bn2dec(ga->x), BN_bn2dec(ga->y), BN_bn2dec(result->x), BN_bn2dec(result->y));
-    }
-    ECdouble(key.group, ret, ga);
-    BIGNUM *scalar = BN_CTX_get(ctx);
-    BN_set_word(scalar, 50);
-    
-    result = ECScalarMult(key.group, scalar, ret);
-    printf("Result Multiply { %s, %s }\n", BN_bn2dec(result->x), BN_bn2dec(result->y));
-    */
     BN_CTX_end(ctx);
     BN_CTX_free(ctx);
-
     return 0;
 }
