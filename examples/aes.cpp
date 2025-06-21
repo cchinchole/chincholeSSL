0c5b353eb5251066248ab5f246b8473141ca43f4
7995419+cchinchole@users.noreply.github.com <7995419+cchinchole@users.noreply.github.com>
2025-06-20 21:40:34 -0500

    Updates Instead of a single main executable for example, broke down examples into their own files within the examples/ directory Rewrote the Makefiles to be more modular

---
+++ examples/aes.cpp
@@ -0,0 +1,53 @@
+#include "../inc/crypto/aes.hpp"
+#include <memory.h>
+#include "../inc/utils/bytes.hpp"
+
+int main() {
+  AES_CTX *ctx = new AES_CTX();
+  ctx->mode = AES_CBC_128;
+
+  char aes_kat_key[] = "2b7e151628aed2a6abf7158809cf4f3c";
+  char aes_iv_key[] = "000102030405060708090a0b0c0d0e0f";
+
+  char cbc_kat[] =
+      "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46"
+      "a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710";
+
+  char ctr_iv[] = "f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+
+  FIPS_197_5_2_KeyExpansion(ctx, scanHex(aes_kat_key, 128 / 8));
+  SetIV(ctx, scanHex(aes_iv_key, 16));
+  uint8_t *outA = (uint8_t *)malloc(64);
+  uint8_t *outB = (uint8_t *)malloc(64);
+
+  CBC_Encrypt(ctx, outA, scanHex(cbc_kat, 64), 64);
+  printf("CBC Encrypt: %s\n", printWord(outA, 64, 16));
+  CBC_Decrypt(ctx, outB, outA, 64);
+  printf("CBC Decrypt: %s\n", printWord(outB, 64, 16));
+  if (!memcmp(scanHex(cbc_kat, 64), outB, 64))
+    printf("CBC passed!\n");
+  else
+    printf("CBC failed.\n");
+
+  memset(outA, 0, 64);
+  memset(outB, 0, 64);
+
+  SetIV(ctx, scanHex(ctr_iv, 16));
+  CTR_xcrypt(ctx, outA, scanHex(cbc_kat, 64), 64);
+  printf("CTR Encrypt: %s\n", printWord(outA, 64, 16));
+
+  CTR_xcrypt(ctx, outB, outA, 64);
+  printf("CTR Decrypt: %s\n", printWord(outB, 64, 16));
+  if (!memcmp(scanHex(cbc_kat, 64), outB, 64))
+    printf("CTR Decrypt passed!\n");
+  else
+    printf("CTR Decrypt failed.\n");
+
+  for (int i = 0; i < 64; i++) {
+    if (outB[i] != scanHex(cbc_kat, 64)[i]) {
+      printf("Failure found on %d: %02x %02x\n", i, outB[i],
+             scanHex(cbc_kat, 64)[i]);
+    }
+  }
+  return 0;
+}
---
+++ examples/ec.cpp
@@ -0,0 +1,58 @@
+#include "../inc/crypto/ec.hpp"
+#include <openssl/bn.h>
+
+int main() {
+   
+  /* Assigning to NULL as they will be generated later */
+  cECKey *key = NULL;
+  cECSignature *sig = NULL;
+  cECKey *myKey2 = new cECKey();
+  cECSignature *mySig2 = new cECSignature();
+
+  char *msg = "Hello World";
+
+  if (key == NULL) {
+    key = new cECKey();
+    FIPS_186_4_B_4_2_KeyPairGeneration(key);
+  }
+
+  if (sig == NULL) {
+    sig = new cECSignature();
+    if (FIPS_186_5_6_4_1_GenerateSignature(sig, msg, key) != 0)
+      printf("Failed to generate signature\n");
+  }
+
+  FIPS_186_4_B_4_2_KeyPairGeneration(myKey2);
+
+  if (FIPS_186_5_6_4_1_GenerateSignature(mySig2, msg, myKey2) != 0)
+    printf("Failed to generate signature\n");
+
+  /* Return code of 0 indicates signature match succeeded */
+  printf("Verifying against correct signature: %s\n",
+         FIPS_186_5_6_4_2_VerifySignature(sig, msg, key->group, key->pub) == 0
+             ? "Passed!"
+             : "Failed!");
+  /* Return code of -1 indicates signature match failed */
+  printf("Verifying against wrong signature: %s\n",
+         FIPS_186_5_6_4_2_VerifySignature(mySig2, msg, key->group, key->pub) ==
+                 -1
+             ? "Passed!"
+             : "Failed!");
+
+  /* Return code of -1 indicates signature match failed */
+  printf("Verifying against wrong key: %s\n",
+         FIPS_186_5_6_4_2_VerifySignature(sig, msg, myKey2->group,
+                                          myKey2->pub) == -1
+             ? "Passed!"
+             : "Failed!");
+  char foobar[] = "sdfsdfsdfsdfsd0xx00x0z98z8882828kzzkzkzku2228828";
+
+  /* Return code of -1 indicates signature match failed */
+  printf("Verifying against wrong message: %s\n",
+         FIPS_186_5_6_4_2_VerifySignature(sig, foobar, key->group, key->pub) ==
+                 -1
+             ? "Passed!"
+             : "Failed!");
+
+  return 0;
+}
